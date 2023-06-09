581a1cb83a509c122099624a93b0f4afe9e9bed0
calixlock <calixlock@naver.com>
2023-06-09 12:11:45 +0900

    add_index+controller

---
+++ member/src/main/java/com/calixlock/member/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.calixlock.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.PublicKey;
+
+@Controller
+public class HomeController {
+    // 기본 페이지 요청
+    @GetMapping("/")
+    public String index(){
+        return "index"; // member\src\main\resources\templates\index.html 찾아감
+    }
+}
