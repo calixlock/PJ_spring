22338dfa7f63468b443160ddd4027c23ae31389a
calixlock <calixlock@naver.com>
2023-06-10 10:30:01 +0900

    sighup_id/pw/name_data_post

--- member/src/main/java/com/calixlock/member/controller/HomeController.java
+++ member/src/main/java/com/calixlock/member/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.calixlock.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.PublicKey;
 
@@ -12,4 +13,5 @@ public class HomeController {
     public String index(){
         return "index"; // member\src\main\resources\templates\index.html 찾아감
     }
+
 }
