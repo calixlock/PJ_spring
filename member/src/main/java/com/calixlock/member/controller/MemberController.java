46219248a8a6b8c61ccce74e676f9b7709c4bdeb
calixlock <calixlock@naver.com>
2023-06-10 19:33:43 +0900

    Add mysql + spring_data_jpa dependency to gradle and set application.yml

--- member/src/main/java/com/calixlock/member/controller/MemberController.java
+++ member/src/main/java/com/calixlock/member/controller/MemberController.java
@@ -1,6 +1,9 @@
 package com.calixlock.member.controller;
 
 import com.calixlock.member.dto.MemberDTO;
+import com.calixlock.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,12 +11,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor // 객체 주입 방식
 public class MemberController {
+    // 생성자 주입 방식
+    private final MemberService memberService;
 
+    // 회원가입 페이지 출력
     @GetMapping("/member/save")
     public String SaveForm() {
         return "save";
-    }
+    } // resources/templates/save.html 반환
     @PostMapping("/member/save")
     // method 1
 //    public String save(@RequestParam("memberEmail") String memberEmail,
@@ -26,6 +33,7 @@ public class MemberController {
     public String save(@ModelAttribute MemberDTO memberDTO){
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
         return "index";
     }
 }
--- member/src/main/java/com/calixlock/member/service/MemberService.java
+++ member/src/main/java/com/calixlock/member/service/MemberService.java
@@ -1,7 +1,15 @@
 package com.calixlock.member.service;
 
+import com.calixlock.member.controller.MemberController;
+import com.calixlock.member.dto.MemberDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+    public void save(MemberDTO memberDTO) {
+
+    }
 }
